1699f9905429a682338f336ec1b3b34fe2191201
Nathalieelizabeths <elizabethsnathalie@gmail.com>
2023-07-10 08:35:30 +0300

    commit

--- greenkiosk/customer/models.py
+++ greenkiosk/customer/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Customer(models.Model):
@@ -8,6 +9,7 @@ class Customer(models.Model):
     phone = models.CharField(max_length=13)
     email = models.EmailField()
     password = models.CharField(max_length=50)
+    user =models.OneToOneField(User,null=True,on_delete=models.CASCADE)
     def register_customer(self):
         self.save
     def get_customer_email(email):
--- greenkiosk/greenkiosk/settings.py
+++ greenkiosk/greenkiosk/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'review',
     'delivery',
     'cart',
+    'vendor',
 ]
 
 MIDDLEWARE = [
